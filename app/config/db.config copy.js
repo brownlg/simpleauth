4a04285782e1657b55b07a91079a262f6b487252
Lucas <brownlg@hotmail.com>
2021-07-04 11:20:50 -0700

    working

---
+++ app/config/db.config copy.js
@@ -0,0 +1,7 @@
+module.exports = {
+    HOST: "192.168.1.102",
+    PORT: 27017,
+    DB: "user_auth",
+    USER: "serviceaccount",
+    PWD: "xDedg123_qq"
+  };
---
+++ server.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const dbConfig = require("./app/config/db.config.js");
+
+const app = express();
+
+
+
+const db = require("./app/models");
+const Role = db.role;
+
+db.mongoose
+  .connect('mongodb://192.168.1.102:27017/user_auth', {
+    auth:
+    {
+      authdb: dbConfig.DB,
+      user: dbConfig.USER,
+      password: dbConfig.PWD
+    },
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
+function initial() {
+    console.log("Initializing user roles");
+
+    Role.estimatedDocumentCount((err, count) => {
+      if (!err && count === 0) {
+        new Role({
+          name: "user"
+        }).save(err => {
+          if (err) {
+            console.log("error", err);
+          }
+  
+          console.log("added 'user' to roles collection");
+        });
+  
+        new Role({
+          name: "moderator"
+        }).save(err => {
+          if (err) {
+            console.log("error", err);
+          }
+  
+          console.log("added 'moderator' to roles collection");
+        });
+  
+        new Role({
+          name: "admin"
+        }).save(err => {
+          if (err) {
+            console.log("error", err);
+          }
+  
+          console.log("added 'admin' to roles collection");
+        });
+      }
+    });
+  }
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
